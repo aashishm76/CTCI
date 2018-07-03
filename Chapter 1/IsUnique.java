0403e2b3781bbdab8a70bdb6ec11eb1b181dea36
Aashish <aashishm97@knights.ucf.edu>
2018-07-02 23:21:11 -0400

    Completed solution

--- Chapter 1/IsUnique.java
+++ Chapter 1/IsUnique.java
@@ -3,24 +3,46 @@ import java.util.*;
 
 public class IsUnique
 {
-  String entry;
-  int i;
-
   public static void main(String[] args)
   {
+    boolean checker;
+    checker = uniqueString();
+
+    if (checker == true)
+      System.out.println("This string is unique");
+    else{
+      System.out.println("This string is not unique");
+    }
+  }
+
+  public static boolean uniqueString()
+  {
+    String entry;
+    int value;
+    int i;
+
     // Scanner instance to read user input
     Scanner userInput;
     userInput = new Scanner(System.in);
 
     // Get user input
-    System.print.ln("Enter a string: ");
+    System.out.println("Enter a string: ");
     entry = userInput.nextLine();
 
-    System.print.ln(entry);
+    // Convert string to an array
+    char[] arrayentry = entry.toCharArray();
 
-    // Convert the user input to an array
+    // Loop through each letter and if keep a flag thats false as long as each letter is different
+    boolean[] char_set = new boolean[128];
 
+    for (i = 0; i < entry.length(); i++)
+    {
+      value = entry.charAt(i);
+      if(char_set[value])
+        return false;
+      char_set[value] = true;
+    }
 
+    return true;
   }
-
 }
